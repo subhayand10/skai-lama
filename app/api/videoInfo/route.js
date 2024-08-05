c1134542109c147aa5be45bd7203f26bf607ee15
Subhayan Das <subhayan.sd10@gmail.com>
2024-08-05 21:55:07 +0530

    postgres db url update

--- app/api/videoInfo/route.js
+++ app/api/videoInfo/route.js
@@ -1,14 +1,11 @@
-import pool from "@/lib/db";
 import { PrismaClient } from "@prisma/client";
 
 
 const prisma = new PrismaClient();
 export  async function GET(req, res) {
   try {
-    // const users = await prisma.user.findMany();
-    // console.log(typeof users);
-    const { rows } = await pool.query("SELECT * FROM videos");
-    return new Response(JSON.stringify(rows), {
+    const videoInfo = await prisma.videos.findMany();
+    return new Response(JSON.stringify(videoInfo), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
